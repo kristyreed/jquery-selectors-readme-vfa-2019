2ed9ffe9ba15b930bef2d1629ff74339ae64e45b
Kristy Reed <kristy.reed22@gmail.com>
2019-06-16 11:05:32 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,5 +8,21 @@
 //$('div:last')
 
 function paragraphSelector() {
-  $('p')
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child')
 }
